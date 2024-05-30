d28a93aeee4d591408794baa3301b8e604caa171
Renato Budiman <renato.budiman@gms.church>
2024-05-30 23:44:41 +0700

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #/usr/bin/bash
 
-export TAG=latest
-docker compose -f docker-compose-run.yaml build && docker compose -f docker-compose-run.yaml push
+export TAG=lite-0.6
+docker compose -f docker-compose-run.yaml build
+docker compose -f docker-compose-run.yaml push
+# docker compose -f docker-compose-run.yaml up
